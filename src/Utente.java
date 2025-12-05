44dad482fe2605b640688660c3267d20d21e9f60
Simone Restivo <60784305+unessere@users.noreply.github.com>
2025-12-05 12:23:51 +0100

    Aggiunta classe Utente

---
+++ src/Utente.java
@@ -0,0 +1,25 @@
+
+
+public class Utente {
+	private String username;
+	private String password;
+	
+	public Utente(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	//versione semplice senza hash
+	public String getPassword() {
+		return password;
+	}
+	
+	public String toFormattedString() {
+		return username + ";" + password;
+	}
+
+}
